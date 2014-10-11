bff9819a035c4005857fff42ec176f1185611dff
Josh Pollock <jpollock412@gmail.com>
2014-10-10 23:36:04 -0400

    update tab height evener thingy

--- assets/js/src/gus_ui.js
+++ assets/js/src/gus_ui.js
@@ -19,34 +19,43 @@ jQuery(document).ready(function($) {
 
 
     function tabHeight() {
-        var width = $( document ).width();
-
-        if (width > 480) {
+        var width = $(document).width();
+        var divs = '#tabs .content';
+        if (width > 640) {
             var maxHeight = -1;
-            var divs = '#tabs .content';
 
-            if ( undefined != paginatedViews ) {
-                $.each( paginatedViews, function( index, value ){
-                    if ( $( value ).length > 0 ) {
-                        maxHeight = maxHeight > $( value ).height() ? maxHeight : $( value ).height();
-                    };
+
+            if (undefined != paginatedViews) {
+                $.each(paginatedViews, function (index, value) {
+                    if ($(value).length > 0) {
+                        maxHeight = maxHeight > $(value).height() ? maxHeight : $(value).height();
+                    }
+                    ;
                 });
             }
 
-                $(divs).each(function () {
-                    maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-                });
+            $(divs).each(function () {
+                maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+            });
 
 
-            if ( maxHeight > 0 ) {
+            if (maxHeight > 0) {
                 $(divs).each(function () {
                     $(this).height(maxHeight);
                 });
 
-                $('ul.tabs').not( '.consensus-tabs' ).height(maxHeight);
+                $('ul.tabs').height(maxHeight);
             }
         }
+        else {
+
+            $('ul.tabs').removeAttr( 'style' );
+            $(divs).each(function () {
+              $(this).removeAttr( 'style' );
+            });
+        }
     }
+
 
     $( '#ht-sub-menu-button' ).click(function() {
         $( this ).toggleClass( 'expanded' ).siblings( 'div' ).slideToggle();
